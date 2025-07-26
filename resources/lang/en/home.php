0d28ff16e225850d2d464d26cef19628de1cfd1a
Nisma95 <nsma22k@gmail.com>
2025-07-26 18:22:47 +0300

    update app

--- resources/lang/en/home.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'title' => 'First Draft',
-    'welcome_message' => 'Welcome',
-    'welcome_split' => ['Welcome', 'to', 'the', 'First', 'Draft'],
-    'start_now' => 'Start Now',
-];
